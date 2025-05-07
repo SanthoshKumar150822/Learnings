a77bff7ac9f732639aff4fa4d8127604c1bed9aa
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-07 09:32:21 +0530

    Update Factorial.java

--- Java/Coding Questions/Factorial.java
+++ Java/Coding Questions/Factorial.java
@@ -6,21 +6,28 @@ public class Factorial
 
 	public static void main(String args[])
 	{
-		int i=1, num=0, fact=1, flag=0;
+		int i=1, num=0, fact=1;
+		String a = "";
 		Scanner input = new Scanner(System.in);
-		while(flag==0)
+
+		while(true)
 		{
 			System.out.print("Enter a number:");
 			num = input.nextInt();
-			if(num>=0){ flag++; }
+			if(num>=0){ break;}
 			else{ System.out.println("Invalid number!, enter a Positive number.");}
 		}
-		for(i=1; i<=num; i++)
+
+		for(i=num; i>=1; i--)	//for(i=1; i<=num; i++)
 		{
 			fact*=i;
+			if(i>1){a+=(i+"x");}
+			else{a+=i;}
 		}
 
 		System.out.println("Factorial of "+num+" is "+fact);
+		System.out.print(num+"! ("+ a +") is "+ fact);
+
 	}
 
 }

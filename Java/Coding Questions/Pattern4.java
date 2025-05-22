42cca82d45ab6c504a88f183bed77da67c1677be
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 11:23:57 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern4.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern4
+{
+
+	public static void main(String args[])
+	{
+		int z=5;
+		for(int i=1; i<=5; i++)
+		{
+
+			for(int j=1; j<=z; j++)
+			{
+
+				System.out.print(j+" ");
+
+			}
+			
+			z--;
+			System.out.println();
+
+		} 		
+
+	}
+
+}

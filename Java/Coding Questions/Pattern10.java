982a53f1fcbd8ba4ec38fecb82eff2877107f7f2
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 13:06:56 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern10.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern10
+{
+
+
+	public static void main(String args[])
+	{
+
+		int z=5;
+		for(int i=1; i<=5; i++)
+		{
+
+			for(int j=1; j<=5; j++)
+			{
+
+				System.out.print(z+" ");
+
+			}
+			
+			z--;
+			System.out.println();
+		} 
+
+	}
+
+
+}

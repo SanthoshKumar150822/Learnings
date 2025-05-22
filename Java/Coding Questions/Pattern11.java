2666462757c39ccade7762bacbfc198d086a7ee9
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 13:21:30 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern11.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern11
+{
+
+
+	public static void main(String args[])
+	{
+
+		int z=1;
+		for(int i=1; i<=5; i++)
+		{
+
+			for(int j=1; j<=5; j++)
+			{
+
+				System.out.print(z+" ");
+
+			}
+		
+			z++;
+			System.out.println();
+		}
+
+	}
+
+
+}

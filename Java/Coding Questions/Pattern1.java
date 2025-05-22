7c6ebccd1bee6b0f68a43668dd886bb133d1f5c7
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 09:56:14 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern1
+{
+
+	public static void main(String args[])
+	{
+
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=i; j<=5; j++)
+			{
+
+				System.out.print("* ");
+				//System.out.print(i+" ");
+
+			}
+			
+			System.out.println();
+		}
+	
+	}
+
+
+}

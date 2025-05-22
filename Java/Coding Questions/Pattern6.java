e4bdf3261440597f92df8d96e4e9a036716fc90a
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 11:43:39 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern6.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern6
+{
+
+	public static void main(String args[])
+	{
+
+		int z=1;
+		for(int i=5; i>=1; i--)
+		{
+
+			for(int j=i; j<=5; j++)
+			{
+				System.out.print(z+" ");
+			}
+			z++;
+			System.out.println();
+
+		}
+	
+
+	}
+
+}

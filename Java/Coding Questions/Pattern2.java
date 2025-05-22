cc2c9f2e8c5ce295d89fbf1434ee5a21e8c0bf79
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-22 10:31:04 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern2
+{
+
+	public static void main(String args[])
+	{
+
+		for(int i=5; i>=1; i--)
+		{
+		
+			for(int j=i; j<=5; j++)
+			{
+				System.out.print("* ");
+				//System.out.print(i+" ");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}

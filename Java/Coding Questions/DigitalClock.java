f6d0175f4d04c3759a504f54734a8ecb93caf2c8
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-24 13:33:00 +0530

    Add files via upload

---
+++ Java/Coding Questions/DigitalClock.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class DigitalClock
+{
+
+
+	public static void main(String args[])
+	{
+
+
+		int hr=0, min=0, sec=0;
+		Scanner input = new Scanner(System.in);
+		userInput(hr, min, sec, input);		
+
+	}
+
+
+	public static void userInput(int hr, int min, int sec, Scanner input)
+	{
+
+
+		while(true)
+		{
+
+			System.out.print("Enter Hour:");
+			hr= input.nextInt();
+			System.out.print("Enter Minute:");
+			min = input.nextInt();
+			System.out.print("Enter Second:");
+			sec = input.nextInt();
+			System.out.println();
+
+			if((hr>-1 && hr<24) && (min>-1 && min<60) && (sec>-1 && sec<60))
+			{
+
+				break;
+			
+			}
+			else
+			{
+
+				System.out.println("Invalid values!, Enter values in the range Hr(0-23), Min(0-59), Sec(0-59).");
+
+			}
+
+		}
+		
+		printDigitalTime(hr, min, sec);
+
+	}
+
+
+	public static void printDigitalTime(int hr, int min, int sec)
+	{
+
+
+		while(true)
+		{
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b");
+			System.out.print(hr+":"+min+":"+sec);
+			try{ Thread.sleep(1000); }
+			catch(Exception e){ break; }
+
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b");
+
+
+			sec++;
+
+			if(sec>59){ sec=0; min++; }
+			if(min>59){ min=0; hr++; }
+			if(hr>23){ hr=0; }
+
+			
+
+
+		}
+
+
+	}
+
+
+
+}

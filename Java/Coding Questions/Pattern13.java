0ce13f083c73a5b8f2ff9d7a947a2d37fa3fdeca
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-05-25 12:17:36 +0530

    Add files via upload

---
+++ Java/Coding Questions/Pattern13.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Pattern13
+{
+
+
+	public static void main(String args[])
+	{
+
+
+
+		for(int i=1; i<=5; i++)
+		{
+
+			if(i==1 || i==5)
+			{
+
+				System.out.println("* * * * *");
+
+			}
+			else
+			{
+
+				System.out.println("*       *");			
+
+			}
+
+		}
+
+		/*for(int i=1; i<=5; i++)
+		{
+
+			for(j=1; j<=5; j++)
+			{
+
+				
+
+			}
+
+		}*/
+
+	}
+
+
+}

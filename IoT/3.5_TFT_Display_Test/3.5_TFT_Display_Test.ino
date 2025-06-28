8916766c34ebdf47451172fb3789bf0515245411
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2025-06-28 18:28:34 +0530

    Add files via upload

---
+++ IoT/3.5_TFT_Display_Test/3.5_TFT_Display_Test.ino
@@ -0,0 +1,24 @@
+//Display Check
+#include <MCUFRIEND_kbv.h>
+#include <Adafruit_GFX.h>
+
+MCUFRIEND_kbv tft;
+
+void setup() {
+  Serial.begin(9600);
+  uint16_t ID = tft.readID();
+  Serial.print("TFT ID = 0x");
+  Serial.println(ID, HEX);
+  if (ID == 0xD3D3) ID = 0x9486; // fallback for common shields
+  tft.begin(ID);
+  tft.setRotation(1);
+  tft.fillScreen(TFT_RED);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(40, 100);
+  tft.println("Display OK");
+}
+
+void loop() {}
+
+
